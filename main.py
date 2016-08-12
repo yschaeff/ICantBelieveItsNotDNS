823bd9cfcfef928a291e97452785acf9002a5540
Yuri Schaeffer <yuri@nlnetlabs.nl>
2016-08-12 17:10:28 +0200

    Reboot on memory problems. Allocate less.

--- main.py
+++ main.py
@@ -206,16 +206,13 @@ s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 #bind to first IF. Fine I guess.
 s.bind(socket.getaddrinfo('0.0.0.0', 53)[0][-1])
 
-from webrepl import start
-start()
-
 led = Pin(2, Pin.OUT)
 led.value(0)
 ledstate = 1
 
 while 1: #while not recv packet of death ;)
     try:
-        m, addr = s.recvfrom(1024)
+        m, addr = s.recvfrom(256)
 
         # Toggle a LED
         led.value(ledstate)
@@ -260,10 +257,11 @@ while 1: #while not recv packet of death ;)
             resp += rdata
 
         s.sendto(resp, addr)
-    except OSError as e:
-        print("OSError: {0}".format(e))
-        ## if the query rate is too high the ESP can't keep up
-        ## sleep for a bit and hope for better times
-        sleep(2)
+    except KeyboardInterrupt as e:
+        from webrepl import start
+        start()
+        break
     except Exception as e:
         print("Exception: {0}".format(e), type(e))
+        sleep(2)
+        reset()
